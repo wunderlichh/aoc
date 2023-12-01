4ae9a13ee5c59f114c4e7c50e53538c79634cd00
Hendrik Wunderlich <hendrik.wunderlich@vacom.de>
2023-12-01 13:26:10 +0100

    2023 1_2

--- 2023/1_2.fsx
+++ 2023/1_2.fsx
@@ -10,42 +10,62 @@ zoneight234
 7pqrstsixteen"""
         .Split(null)
 
-let numbers input =
-    seq {
-        for m in Regex.Matches(input, @"(\d|one|two|three|four|five|six|seven|eight|nine)") do
-            yield m.Value
-    }
+let rec startNum (input: string) =
+    if input.Length = 0 then
+        failwith "Start not found"
+    elif input.StartsWith "one" then
+        1
+    elif input.StartsWith "two" then
+        2
+    elif input.StartsWith "three" then
+        3
+    elif input.StartsWith "four" then
+        4
+    elif input.StartsWith "five" then
+        5
+    elif input.StartsWith "six" then
+        6
+    elif input.StartsWith "seven" then
+        7
+    elif input.StartsWith "eight" then
+        8
+    elif input.StartsWith "nine" then
+        9
+    elif System.Char.IsDigit(input[0]) then
+        input[0] |> string |> int
+    else
+        startNum input[1..]
 
-let (|Int|_|) str =
-    match System.Int32.TryParse(str: string) with
-    | (true, int) -> Some(int)
-    | _ -> None
+let rec endNum (input: string) =
+    if input.Length = 0 then
+        failwith "End not found"
+    elif input.EndsWith "one" then
+        1
+    elif input.EndsWith "two" then
+        2
+    elif input.EndsWith "three" then
+        3
+    elif input.EndsWith "four" then
+        4
+    elif input.EndsWith "five" then
+        5
+    elif input.EndsWith "six" then
+        6
+    elif input.EndsWith "seven" then
+        7
+    elif input.EndsWith "eight" then
+        8
+    elif input.EndsWith "nine" then
+        9
+    elif System.Char.IsDigit(input[input.Length - 1]) then
+        input[input.Length - 1] |> string |> int
+    else if input.Length = 1 then
+        endNum ""
+    else
+        endNum (input.Substring(0, input.Length - 1))
 
-let numeric number =
-    match number with
-    | "one" -> 1
-    | "two" -> 2
-    | "three" -> 3
-    | "four" -> 4
-    | "five" -> 5
-    | "six" -> 6
-    | "seven" -> 7
-    | "eight" -> 8
-    | "nine" -> 9
-    | Int x -> x
-    | _ -> failwith "invalid"
+System.IO.File.ReadAllLines "1_1.txt"
+|> Array.map (fun l -> sprintf "%d%d" (startNum l) (endNum l) |> int)
+|> Array.sum
 
-let output =
-    System.IO.File.ReadAllLines "1_1.txt"
-    // test_input
-    |> Array.map numbers
-    |> Array.map (Seq.map numeric)
-    |> Array.map (fun n -> (n |> Seq.head), (n |> Seq.last))
-    |> Array.map (fun t -> sprintf "%d%d" (fst t) (snd t))
-    |> Array.map int
-// |> Seq.map (fun n -> sprintf "%d%d" (n |> Seq.head) (n |> Seq.last))
-// |> Seq.map int
-
-// 1: 54388
-// 53_519 --> WRONG TOO HIGH
-printfn "OUT %A" (output |> Array.sum)
+// 53515
