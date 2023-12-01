853c319bb6abcb743fee56d810289f9770c961c2
Hendrik Wunderlich <hendrik.wunderlich@vacom.de>
2023-12-01 13:30:59 +0100

    2023 1_2

--- 2023/1_2.fsx
+++ 2023/1_2.fsx
@@ -1,5 +1,3 @@
-open System.Text.RegularExpressions
-
 let test_input =
     """two1nine
 eightwothree
@@ -11,52 +9,32 @@ zoneight234
         .Split(null)
 
 let rec startNum (input: string) =
-    if input.Length = 0 then
-        failwith "Start not found"
-    elif input.StartsWith "one" then
-        1
-    elif input.StartsWith "two" then
-        2
-    elif input.StartsWith "three" then
-        3
-    elif input.StartsWith "four" then
-        4
-    elif input.StartsWith "five" then
-        5
-    elif input.StartsWith "six" then
-        6
-    elif input.StartsWith "seven" then
-        7
-    elif input.StartsWith "eight" then
-        8
-    elif input.StartsWith "nine" then
-        9
+    if input.Length = 0             then failwith "Start not found"
+    elif input.StartsWith "one"     then 1
+    elif input.StartsWith "two"     then 2
+    elif input.StartsWith "three"   then 3
+    elif input.StartsWith "four"    then 4
+    elif input.StartsWith "five"    then 5
+    elif input.StartsWith "six"     then 6
+    elif input.StartsWith "seven"   then 7
+    elif input.StartsWith "eight"   then 8
+    elif input.StartsWith "nine"    then 9
     elif System.Char.IsDigit(input[0]) then
         input[0] |> string |> int
     else
         startNum input[1..]
 
 let rec endNum (input: string) =
-    if input.Length = 0 then
-        failwith "End not found"
-    elif input.EndsWith "one" then
-        1
-    elif input.EndsWith "two" then
-        2
-    elif input.EndsWith "three" then
-        3
-    elif input.EndsWith "four" then
-        4
-    elif input.EndsWith "five" then
-        5
-    elif input.EndsWith "six" then
-        6
-    elif input.EndsWith "seven" then
-        7
-    elif input.EndsWith "eight" then
-        8
-    elif input.EndsWith "nine" then
-        9
+    if input.Length = 0             then failwith "End not found"
+    elif input.EndsWith "one"       then 1
+    elif input.EndsWith "two"       then 2
+    elif input.EndsWith "three"     then 3
+    elif input.EndsWith "four"      then 4
+    elif input.EndsWith "five"      then 5
+    elif input.EndsWith "six"       then 6
+    elif input.EndsWith "seven"     then 7
+    elif input.EndsWith "eight"     then 8
+    elif input.EndsWith "nine"      then 9
     elif System.Char.IsDigit(input[input.Length - 1]) then
         input[input.Length - 1] |> string |> int
     else if input.Length = 1 then
